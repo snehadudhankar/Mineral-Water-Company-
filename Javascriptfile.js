493ffe1105e8951eb9b7518a2e8b461f993e510d
Sneha Dudhankar <sneha2017dudhankar@gmail.com>
2021-08-18 00:35:38 +0530

    First Commit

---
+++ Javascriptfile.js
@@ -0,0 +1,13 @@
+
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+  container: 'body'
+})
